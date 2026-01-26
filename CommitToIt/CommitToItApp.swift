06f00b791c25813ed2f9fb641795764f78ff86da
Jonathan Malave <jmaxmalave@aol.com>
2026-01-25 21:56:09 -0500

    Initial Commit

---
+++ CommitToIt/CommitToItApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitToItApp.swift
+//  CommitToIt
+//
+//  Created by Jonathan Malave on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CommitToItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
