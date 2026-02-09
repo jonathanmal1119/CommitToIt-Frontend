a5b4e16661cffcab83eaca95dccaf72867247958
Jonathan Malave <jmaxmalave@aol.com>
2026-02-08 20:54:19 -0500

    Rewards Work with History View

---
+++ CommitToIt/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  CommitToIt
+//
+//  Created by Jonathan Malave on 2/8/26.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
